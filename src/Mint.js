8f6e57e0a37ddf14d51b2eea8e76acd9dfc9da46
lucas.vasconcelos@imaginebeyond.com.br <lucas.vasconcelos@imaginebeyond.com.br>
2019-09-28 06:30:42 -0300

    ble

--- src/Mint.js
+++ src/Mint.js
@@ -5,7 +5,7 @@ import { Table } from "antd";
 
 const Mint = () => {
   const ContextValue = React.useContext(BadgerContext);
-  const { wallet,tokens } = ContextValue;
+  const { wallet, tokens } = ContextValue;
   const columns = [
     {
       title: "Id",
@@ -16,23 +16,18 @@ const Mint = () => {
     {
       title: "Action",
       key: "action",
-      render: (text) => (
+      render: text => (
         <span>
-          <a onClick={() => mintToken(wallet, { tokenId: text, qty: 1 })}>Mint</a>
+          <a onClick={() => {
+              mintToken(wallet, { tokenId: text.tokenId, qty: 1 })
+          }}>
+            Mint
+          </a>
         </span>
       )
     }
   ];
-  return (
-<Table columns={columns} dataSource={tokens} />
-  );
+  return <Table columns={columns} dataSource={tokens} />;
 };
 
 export default Mint;
-
-
-
-
-
-
-
