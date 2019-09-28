2baa2113600d02dd8b185ab38f81d99da7681c6d
lucas.vasconcelos@imaginebeyond.com.br <lucas.vasconcelos@imaginebeyond.com.br>
2019-09-28 00:51:59 -0300

    config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react';
-import Button from 'antd/es/button';
-import './App.css';
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    );
-  }
-}
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const App = () => {
+  const [collapsed, setCollapesed] = React.useState(false);
+
+  return (
+    <div className="App">
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={() => setCollapesed(!collapsed)}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff", padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
 
 export default App;
