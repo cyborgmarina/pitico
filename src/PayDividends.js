aac6612887971be1e34e2755425ba9a4ed0d1142
lucas.vasconcelos@imaginebeyond.com.br <lucas.vasconcelos@imaginebeyond.com.br>
2019-09-28 04:05:37 -0300

    ajustes

---
+++ src/PayDividends.js
@@ -0,0 +1,11 @@
+import React from "react";
+const PayDividends = () => {
+    return (
+      <div>
+        <h2>PayDividends</h2>
+      </div>
+    );
+  };
+  
+  export default PayDividends;
+  
