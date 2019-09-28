f68bba5f0341ff63f7c331dd711891672e41b17d
lucas.vasconcelos@imaginebeyond.com.br <lucas.vasconcelos@imaginebeyond.com.br>
2019-09-28 05:50:48 -0300

    ble

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,71 @@
 import React from "react";
+import { BadgerContext } from "./badger/context";
+import { Input, Button, notification, Spin } from "antd";
+import { createToken } from "./badger/createToken";
 const Create = () => {
+  const ContextValue = React.useContext(BadgerContext);
+  console.log("ContextValue", ContextValue);
+  const { wallet } = ContextValue;
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState({
+    tokenName: "",
+    tokenSymbol: "",
+    qty: ""
+  });
+
+  //   createToken(w, { tokenName: "hahaha", tokenSymbol: "hahaha", qty: 1000 });
+  async function handleCreateToken() {
+    setLoading(true);
+    const { propstokenName, tokenSymbol, qty } = data;
+    console.log("data", data);
+    try {
+      await createToken(wallet, {
+        propstokenName,
+        tokenSymbol,
+        qty
+      });
+
+      notification.success({
+        message: "Success",
+        description: "Create Token Success"
+      });
+    } catch (e) {
+      notification.error({
+        message: "Error",
+        description: `${e.message}`
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleChange = e => {
+    const { value, name } = e.target;
+
+    setData(p => ({ ...p, [name]: value }));
+  };
   return (
-    <div>
-      <h2>Create</h2>
-    </div>
+    <Spin spinning={loading}>
+      <div style={{ padding: "12px 30% 64px 30%" }}>
+        <h2>Create</h2>
+        <div>
+          <Input
+            placeholder="tokenName"
+            name="tokenName"
+            onChange={e => handleChange(e)}
+          />
+          <Input
+            placeholder="tokenSymbol"
+            name="tokenSymbol"
+            onChange={e => handleChange(e)}
+          />
+          <Input placeholder="qty" name="qty" onChange={e => handleChange(e)} />
+          <div style={{ paddingTop: "12px" }}>
+            <Button onClick={() => handleCreateToken()}>Create Token</Button>
+          </div>
+        </div>
+      </div>
+    </Spin>
   );
 };
 
