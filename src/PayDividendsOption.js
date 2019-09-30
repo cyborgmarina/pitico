efc32297ba7c9661cbaac5faea39bbbb4d535204
Andre Cabrera <andrecabrera@protonmail.ch>
2019-09-29 23:21:47 -0300

    feat - dividends

---
+++ src/PayDividendsOption.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+export default ({ onClick }) => {
+	return (<div style={{ cursor: 'Pointer' }} onClick={onClick}><Icon type="dollar" /> Pay Dividends</div>);
+}
