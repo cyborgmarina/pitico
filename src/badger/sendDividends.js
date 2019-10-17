0e09407d9b7b7662612084894e59fc8b63d078a0
Natan Camargos <natan.camargos@imaginebeyond.com.br>
2019-10-17 01:50:56 -0300

    fix dividends

--- src/badger/sendDividends.js
+++ src/badger/sendDividends.js
@@ -4,42 +4,41 @@
 
 import SLPSDK from "slp-sdk";
 import { sendBch } from "./sendBch";
-import getSlpInstance from './getSlpInstance';
-let Utils = require('slpjs').Utils;
+import getSlpInstance from "./getSlpInstance";
+let Utils = require("slpjs").Utils;
 
 // Set NETWORK to either testnet or mainnet
-const NETWORK = process.env.REACT_APP_NETWORK
+const NETWORK = process.env.REACT_APP_NETWORK;
 
 // Instantiate SLP based on the network.
 const SLP = getSlpInstance(NETWORK);
 
-export async function balancesForToken(tokenId) {
+export async function outputsForToken(tokenId) {
   try {
     const balances = await SLP.Utils.balancesForToken(tokenId);
     return balances;
   } catch (err) {
-    console.error(`Error in getTokenInfo: `, err)
-    throw err
+    console.error(`Error in getTokenInfo: `, err);
+    throw err;
   }
 }
 
 export const sendDividends = async (wallet, { value, tokenId }) => {
-    const balances = await balancesForToken(tokenId);
+  const outputs = await outputsForToken(tokenId);
+  const addresses = [];
+  const values = [];
+  const total = outputs.reduce((p, c) => c.tokenBalance + p, 0);
 
-    const total = balances.reduce((p, c) => c.tokenBalance + p , 0);
-
-    for (let i = 0; i < balances.length; i++) {
-      const balance = balances[i];
-      const address = Utils.toCashAddress(balance.slpAddress);
-      if (address === wallet.cashAddress) {
-        continue;
-      }
-
-      try {
-        return await sendBch(wallet, {value: value * (balance.tokenBalance/total), address: address});
-      } catch(error) {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        i--;
-      }
+  for (let i = 0; i < outputs.length; i++) {
+    const output = outputs[i];
+    const address = Utils.toCashAddress(output.slpAddress);
+    if (address === wallet.cashAddress) {
+      continue;
     }
+
+    addresses.push(address);
+    values.push((output.tokenBalance / total) * value);
+  }
+
+  sendBch(wallet, { addresses, values });
 };
